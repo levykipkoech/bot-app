8f373fe30ff7c8aa5e7a4d0bfd65a56407af33bb
Levy <kipkoechlevy096@gmail.com>
2023-01-23 11:56:03 +0300

    updated css

--- src/components/Bot-colletion.js
+++ src/components/Bot-colletion.js
@@ -58,13 +58,16 @@ function BotCollection(props) {
                 onClick={() => handleClick(bot)}
                   key={bot.id}>
                     <img className='bot-img' src={bot.avatar_url} alt={bot.name} />
+                    <div className='divtwo'>
                     <h5 className='bot-name'>{bot.name}<FontAwesomeIcon icon={classIcons[bot.bot_class]} /></h5>
-                    <h6 className='bot-catchphrace'>{bot.catchphrase}</h6>
-                    <p className='bot-armor'><FontAwesomeIcon icon={faShieldAlt} />{bot.armor}</p>
-                    <p className='bot-damage'>{bot.damage}</p>
-                    <p className='bot-health'><FontAwesomeIcon icon={faHeart} />{bot.health}</p>
+                    
+                          <h6 className='bot-catchphrace'>{bot.catchphrase}</h6>
+                          <p className='bot-armor'><FontAwesomeIcon icon={faShieldAlt} />{bot.armor}</p>
+                          <p className='bot-damage'>{bot.damage}</p>
+                          <p className='bot-health'><FontAwesomeIcon icon={faHeart} />{bot.health}</p>
                      
                     <button className='delete-bot' onClick={() => handleDelete(bot)}>x</button>
+                    </div> 
                 </div>
                 
             ))}
